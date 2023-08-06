3f97d67c12f39148823080dca0731dedae1f84f9
mie-h <miehaga@berkeley.edu>
2023-08-06 14:07:34 -0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# passing-event-data-ecs-task
+# Passing S3 PutObject event data to ECS task
+
+![](images/s3_eventbridge_ecs.drawio.png)
+
+# Scenario
+EventBridge listens to S3 PutObject event when a file is uploaded to S3. This EventBridge rule triggers and starts a new ECS task and passes the s3 key(i.e. filename path) to the ECS task as an environment variable. 
+
+# Prerequisites
+* EventBridge Rule has to be created first. Follow [this link](https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/CloudWatch-Events-tutorial-ECS.html) to create and configure EventBridge Rule.
+* AWS CloudTrail has to be created to listen to event in S3 and log.
+* ECS task and S3 bucket
+
+# How to do it
+In order to pass s3 key to ECS task as an environment variable, EventBridge Rule target has to be configured. This configuration is not possible thtough AWS console. Use [put_targets](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/events/client/put_targets.html).
+See update_ecs_task.py for more detail.
+
+
+# Things I learned
+It is very helpful to check CloudTrail logs for debugging.
+
+
+# Acknowledgments
+* [Run an Amazon ECS Task When a File is Uploaded to an Amazon S3 Bucket](https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/CloudWatch-Events-tutorial-ECS.html)
+* [repost.aws](https://repost.aws/questions/QUtllxTXjgRCeBMmy4MzgNpQ/how-to-convince-eventbridge-to-pass-s3-event-information-to-an-ecs-target)
+* [put_targets on Boto3 doc](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/events/client/put_targets.html)
+* [Passing event data to AWS ECS Task](https://www.linkedin.com/pulse/passing-event-data-aws-ecs-task-roshan-shetty/)
--- update_ecs_task.py
+++ update_ecs_task.py
@@ -7,17 +7,17 @@ import json
 import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read("config.ini")
 
-RULE_NAME = config.get('AWS', 'RULE_NAME')
-REGION = config.get('AWS', 'REGION')
-ACCOUNT_ID = config.get('AWS', 'ACCOUNT_ID')
-CLUSTER_NAME = config.get('AWS', 'CLUSTER_NAME')
-SERVICE_ROLE_NAME = config.get('AWS', 'SERVICE_ROLE_NAME')
-CONTAINER_NAME = config.get('AWS', 'CONTAINER_NAME')
-TASK_DEFINITION = config.get('AWS', 'TASK_DEFINITION')
-SUBNETS = json.loads(config.get('AWS', 'SUBNETS'))
-SECURITY_GROUPS = json.loads(config.get('AWS', 'SECURITY_GROUPS'))
+RULE_NAME = config.get("AWS", "RULE_NAME")
+REGION = config.get("AWS", "REGION")
+ACCOUNT_ID = config.get("AWS", "ACCOUNT_ID")
+CLUSTER_NAME = config.get("AWS", "CLUSTER_NAME")
+SERVICE_ROLE_NAME = config.get("AWS", "SERVICE_ROLE_NAME")
+CONTAINER_NAME = config.get("AWS", "CONTAINER_NAME")
+TASK_DEFINITION = config.get("AWS", "TASK_DEFINITION")
+SUBNETS = json.loads(config.get("AWS", "SUBNETS"))
+SECURITY_GROUPS = json.loads(config.get("AWS", "SECURITY_GROUPS"))
 
 try:
     client = boto3.client("events")
@@ -27,22 +27,29 @@ try:
             {
                 "Id": "1",
                 "Arn": f"arn:aws:ecs:{REGION}:{ACCOUNT_ID}:cluster/{CLUSTER_NAME}",
+                # On top of the permission assigend when creating the rule here:https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/CloudWatch-Events-tutorial-ECS.html,
+                # Assign permissin to access ECS and CloudWatch log
                 "RoleArn": f"arn:aws:iam::{ACCOUNT_ID}:role/service-role/{SERVICE_ROLE_NAME}",
+                # This is the part that links S3 event data and target
                 "InputTransformer": {
                     "InputPathsMap": {
                         "keyname": "$.detail.requestParameters.key",
                         "eventname": "$.detail.eventName",
                     },
-                    "InputTemplate": '{"containerOverrides": [{"name":"' + CONTAINER_NAME + '","environment":[{"name":"EVENTNAME","value":<eventname>},{ "name":"S3_KEY","value":<keyname> }]}]}',
+                    "InputTemplate": '{"containerOverrides": [{"name":"'
+                    + CONTAINER_NAME
+                    + '","environment":[{"name":"EVENTNAME","value":<eventname>},{ "name":"S3_KEY","value":<keyname> }]}]}',
                 },
                 "EcsParameters": {
                     "TaskDefinitionArn": f"arn:aws:ecs:{REGION}:{ACCOUNT_ID}:task-definition/{TASK_DEFINITION}",
                     "TaskCount": 1,
                     "LaunchType": "FARGATE",
                     "NetworkConfiguration": {
+                        # same as Subnets and SG the ECS task you create is in
                         "awsvpcConfiguration": {
                             "Subnets": SUBNETS,
                             "SecurityGroups": SECURITY_GROUPS,
+                            # make sure this is enabled
                             "AssignPublicIp": "ENABLED",
                         }
                     },
